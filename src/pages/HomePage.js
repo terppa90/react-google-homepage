0b890c4728efa60a06149f30b5bcf91b80ca8192
Tero Kaihula <N0465@student.jamk.fi>
2021-09-29 16:29:58 +0300

    react-google-homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from '../styles/center';
+
+const HomePage = () => {
+  return (
+    <Container>
+      <h1>home Page</h1>
+    </Container>
+  );
+};
+
+export default HomePage;
